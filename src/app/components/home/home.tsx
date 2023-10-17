5bae71eed23b4f9ef411d3c11f44142880d2bb62
ipaiva92 <ipaiva92@outlook.com>
2023-10-17 18:02:53 -0300

    feat: Login create

--- src/app/components/home/home.tsx
+++ src/app/components/home/home.tsx
@@ -1,10 +1,26 @@
+"use client";
 import variables from "@/app/variables.module.scss";
 import Image from "next/image";
 import "./home.scss";
 import loginRoute from "@/app/routes/post";
+import { SetStateAction, useState } from "react";
 
 export default function Home() {
-  loginRoute("igao@teste.com", "blimbimds");
+  const [mailValue, setMailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const handleUserChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setMailValue(e.target.value);
+  };
+
+  const handlePasswordChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setPasswordValue(e.target.value);
+  };
+
   return (
     <div>
       <div
@@ -27,9 +43,26 @@ export default function Home() {
       >
         <div className="container-input">
           <p>Logar</p>
-          <input></input>
-          <input></input>
-          <button>Entrar</button>
+          <input
+            type="text"
+            id="inputElement"
+            value={mailValue}
+            onChange={handleUserChange}
+          ></input>
+          <input
+            type="text"
+            id="inputElement"
+            value={passwordValue}
+            onChange={handlePasswordChange}
+          ></input>
+          <button
+            style={{ backgroundColor: variables.primaryBtnColor }}
+            onClick={() => {
+              loginRoute(mailValue, passwordValue);
+            }}
+          >
+            Entrar
+          </button>
           <button>Conecte-se</button>
         </div>
       </div>
--- src/app/routes/post.ts
+++ src/app/routes/post.ts
@@ -4,7 +4,9 @@ const loginRoute = (email: string, password: string) => {
 axios.post("/User/Login", {email, password})
   .then((response) => {
     console.log( response.data);
-  })
+  }).catch((error) => {
+    console.error('POST Request Error:', error);
+  });
 }
 
 export default loginRoute;
