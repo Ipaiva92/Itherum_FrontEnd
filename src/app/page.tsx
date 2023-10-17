d60a65bf2034f34c4397e697041f7f2be8f4a4ae
ipaiva92 <ipaiva92@outlook.com>
2023-10-17 14:03:49 -0300

    feat: add template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
 import variables from "./variables.module.scss";
+import Image from "next/image";
+import "./login.scss";
 
 export default function Home() {
   return (
     <div>
-      <div style={{ backgroundColor: variables.backgroundColorWhite }}>
-        Home
+      <div
+        className="container"
+        style={{ backgroundColor: variables.backgroundColorWhite }}
+      >
+        <Image
+          src="/images/Logo_Itherum_azul.svg"
+          alt="Itherum_Logo"
+          width={406}
+          height={344.72}
+        ></Image>
       </div>
-      <div style={{ backgroundColor: variables.backgroundColor }}>himome</div>
+      <div
+        className="container"
+        style={{
+          backgroundColor: variables.backgroundColor,
+          color: variables.whiteColor,
+        }}
+      ><div className="container-input">
+        <p>Logar</p>
+        <input></input>
+        <input></input>
+        <button>Entrar</button>
+        <button>Conecte-se</button>
+        </div>
+      </div>
     </div>
   );
 }
