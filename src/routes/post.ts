22610543de59b7eb3d13b79fb79176cd77122327
ipaiva92 <ipaiva92@outlook.com>
2023-10-19 22:48:13 -0300

    Fix: router push

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,14 +1,18 @@
 import { api, isAxiosError } from "@/axiosConfig";
 
-const loginRoute = async (email: string, password: string) => {
+const loginRoute = async (email: string, password: string) : Promise<boolean> => {
   try {
     const response = await api.post("/User/Login", { email, password });
-    console.log(response?.data)
+    const jwt = response?.data.jwt;
+
+    localStorage.setItem("token", jwt);
+    return true
   } catch (err) {
     if (isAxiosError(err)) {
       console.error(err.response?.data);
     }
   }
+  return false
 };
 
 export default loginRoute;
