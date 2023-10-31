048e544db238d4f3cb58e9ed82e8fa3d52c77e7f
ipaiva92 <ipaiva92@outlook.com>
2023-10-31 14:29:03 -0300

    feat: react icons

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 import variables from "@/app/variables.module.scss";
 import "./dashboard.scss";
+import Image from "next/image";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Dashboard() {
   return (
@@ -9,7 +13,29 @@ export default function Dashboard() {
         className="topNavBar"
         style={{ backgroundColor: variables.secondaryColor }}
       >
-        Teste
+        <button
+          className="navBarMenuBtn"
+          style={{ backgroundColor: variables.primaryColorVariant }}
+          onClick={() => {}}
+        >
+          <AiOutlineMenu color="white" size="20" />
+        </button>
+        <Image
+          className="logo"
+          src="/images/Logo_Itherum_azul_pequena.svg"
+          alt="Itherum_Logo"
+          width={63}
+          height={38.35}
+        ></Image>
+        <button
+          className="logout"
+          onClick={() => {
+            localStorage.removeItem("userId");
+          }}
+        >
+          <RiLogoutBoxRLine />
+          Log out
+        </button>
       </div>
       <div
         className="leftNavBar"
