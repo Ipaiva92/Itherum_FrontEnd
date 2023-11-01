ba44d802df899b717572ca22609006d4c294546a
ipaiva92 <ipaiva92@outlook.com>
2023-11-01 13:51:31 -0300

    fix: some issues on menu

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,46 +4,67 @@ import "./dashboard.scss";
 import Image from "next/image";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 
 export default function Dashboard() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleMenu = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <div>
-      {" "}
-      <div
-        className="topNavBar"
-        style={{ backgroundColor: variables.secondaryColor }}
-      >
-        <button
-          className="navBarMenuBtn"
-          style={{ backgroundColor: variables.primaryColorVariant }}
-          onClick={() => {}}
-        >
-          <AiOutlineMenu color="white" size="20" />
-        </button>
-        <Image
-          className="logo"
-          src="/images/Logo_Itherum_azul_pequena.svg"
-          alt="Itherum_Logo"
-          width={63}
-          height={38.35}
-        ></Image>
-        <button
-          className="logout"
-          onClick={() => {
-            localStorage.removeItem("userId");
-          }}
-        >
-          <RiLogoutBoxRLine />
-          Log out
-        </button>
-      </div>
-      <div
-        className="leftNavBar"
-        style={{ backgroundColor: variables.primaryColorVariant }}
-      >
-        <a>Teste</a>
-        <a>Teste</a>
-        <a>Teste</a>
+      <header style={{ backgroundColor: variables.secondaryColor }}>
+        <nav className="navbar">
+          <button
+            className={`navBarMenuBtn  main-content ${
+              collapsed ? "collapsed" : ""
+            }`}
+            style={{ backgroundColor: variables.primaryColorVariant }}
+            onClick={toggleMenu}
+          >
+            <AiOutlineMenu color="white" size="20" />
+          </button>
+          <div className="logo">
+            <Image
+              src="/images/Logo_Itherum_azul_pequena.svg"
+              alt="Itherum_Logo"
+              width={63}
+              height={38.35}
+            ></Image>
+          </div>
+          <ul className="nav-list">
+            <li>
+              <a href="#contact">
+                <button
+                  className="logout"
+                  onClick={() => {
+                    localStorage.removeItem("userId");
+                  }}
+                >
+                  <RiLogoutBoxRLine />
+                  Log out
+                </button>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div style={{backgroundColor: variables.primaryColorVariant}} className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+        <a href="#home" className="active">
+          <i className="fa fa-home"></i> <span>Home</span>
+        </a>
+        <a href="#about">
+          <i className="fa fa-info-circle"></i> <span>About</span>
+        </a>
+        <a href="#services">
+          <i className="fa fa-cogs"></i> <span>Services</span>
+        </a>
+        <a href="#contact">
+          <i className="fa fa-envelope"></i> <span>Contact</span>
+        </a>
       </div>
     </div>
   );
